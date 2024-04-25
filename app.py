9d303301c3866ae9591df17f7d9d636035e8cb46
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-25 18:39:26 +0400

    add a simple PoC with FreeSWITCH and mod_audio_stream

--- app.py
+++ app.py
@@ -19,18 +19,20 @@ log_level = config.get('LOG_LEVEL', 'INFO')
 log_level = log_level_mapping.get(log_level.upper(), logging.INFO)
 
 logger = logging.getLogger('freeswitch_audio_stream_poc')
-logger.setLevel(log_level)
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=log_level)
 
 log_console_handler = logging.StreamHandler(sys.stdout)
 log_console_handler.setLevel(log_level)
+log_console_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s'))
 logger.addHandler(log_console_handler)
 
 if config.get('LOG_TO_FILE', 'False'):
     log_file = config.get('LOG_FILE', 'fs_esl.log')
     log_file_handler = logging.FileHandler(log_file)
     log_file_handler.setLevel(log_level)
+    log_file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(message)s'))
     logger.addHandler(log_file_handler)
 
 
@@ -51,11 +53,67 @@ def connect_to_freeswitch():
         logger.info('Connected to FreeSWITCH')
         return con
 
-    logger.error('Error: Could not connect to FreeSWITCH!')
+    logger.error('Could not connect to FreeSWITCH!')
     return None
 
 
-esl_conn = connect_to_freeswitch()
-if esl_conn is None:
-    logger.error('Exiting...')
-    sys.exit(1)
+def originate_call(esl_connection, extension):
+    """
+    Initiates an outbound call from FreeSWITCH to a specified extension.
+
+    This method uses an existing ESLconnection object to originate an outbound
+    call to the specified extension on the FreeSWITCH server.
+
+    Args:
+        esl_connection (ESLconnection): An existing connection object to the FreeSWITCH server.
+        extension (str): The extension number to call.
+
+    Returns:
+        str or None: The UUID of the call if successfully originated, otherwise None.
+    """
+    response = esl_connection.api(
+        "originate", f"user/{extension} &echo() async")
+    if response:
+        # uuid = response.getBody().decode().split(' ')[1]
+        uuid = response.getBody().split()[1]
+        return uuid
+
+    logger.error('Could not originate call')
+    return None
+
+
+def connect_channel_with_ws_endpoint(esl_conn, uuid, ws_endpoint):
+    """
+    Connects a channel to a WebSocket (WS) endpoint for audio streaming.
+
+    This method connects a channel identified by its UUID to the specified
+    WebSocket (WS) endpoint for audio streaming. It sends a command to FreeSWITCH
+    via the provided ESLconnection object to establish the audio stream.
+
+    Args:
+        esl_conn (ESLconnection): An existing connection object to the FreeSWITCH server.
+        uuid (str): The UUID of the channel to connect.
+        ws_endpoint (str): The WebSocket (WS) endpoint URL for audio streaming.
+
+    Returns:
+        None
+    """
+    command = f'uuid_audio_stream {uuid} start {ws_endpoint} stereo 8k'
+    esl_conn.api(command)
+
+
+def main():
+    """
+    Main function
+    """
+    esl_conn = connect_to_freeswitch()
+    if esl_conn is None:
+        logger.error('Exiting...')
+        sys.exit(1)
+
+    uuid = originate_call(esl_conn, config.get('SIP_EXTENSION'))
+    connect_channel_with_ws_endpoint(esl_conn, uuid, config.get('WS_ENDPOINT'))
+
+
+if __name__ == "__main__":
+    main()
