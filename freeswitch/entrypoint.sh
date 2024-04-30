59d998de700bb9ed48dcd9314c1835044ba1b22e
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-30 13:12:05 +0400

    add extension to the dialplan dynamically in entrypoint.sh

--- freeswitch/entrypoint.sh
+++ freeswitch/entrypoint.sh
@@ -2,4 +2,29 @@
 
 sed -i '/<X-PRE-PROCESS cmd="set" data="default_password=[^"]*"\/>/c\<X-PRE-PROCESS cmd="set" data="default_password='"${EXTENSION_PASSWORD:-extensionpassword}"'"\/>' /usr/local/freeswitch/conf/vars.xml
 
+read -r -d '' insertionText << EOM
+<extension name="esl_inbound">
+  <condition field="destination_number" expression="^${EXTENSION_TO_DIAL:-88888}$">
+    <action application="socket" data="${ESL_APP_HOST:-esl-app}:${ESL_APP_PORT:-8022} async full"/>
+  </condition>
+</extension>
+EOM
+
+ESL_APP_HOST="${ESL_APP_HOST:-esl-app}"
+ESL_APP_PORT="${ESL_APP_PORT:-8022}"
+
+awk -v host="$ESL_APP_HOST" -v port="$ESL_APP_PORT" '
+/<context name="default">/ {
+    print
+    print ""
+    print "<extension name=\"esl_inbound\">"
+    print "  <condition field=\"destination_number\" expression=\"^88888$\">"
+    print "    <action application=\"socket\" data=\"" host ":" port " async full\"/>"
+    print "  </condition>"
+    print "</extension>"
+    next
+}
+{ print }
+' /usr/local/freeswitch/conf/dialplan/default.xml > /tmp/default.xml && mv /tmp/default.xml /usr/local/freeswitch/conf/dialplan/default.xml
+
 /usr/local/freeswitch/bin/freeswitch -nonat -nf -nc
