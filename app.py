d77f27e9c62a3c9f5b6bc7a3c758de96444f0218
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-30 12:44:35 +0400

    use ESL inbound connections

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import logging
 import sys
 import json
+import socket
+import threading
 from ESL import ESLconnection
 from dotenv import dotenv_values
 
@@ -35,6 +37,54 @@ if config.get('LOG_TO_FILE', 'True'):
     log_file_handler.setFormatter(logging.Formatter(
         '%(asctime)s - %(levelname)s - %(message)s'))
     logger.addHandler(log_file_handler)
+
+
+def handle_esl_connection(client_socket, address):
+    """
+    Handle incoming ESL connection.
+    """
+    logger.info("Got ESL inbound connection at %s", address)
+    conn = ESLconnection(client_socket.fileno())
+
+    conn.events("plain", "ALL")
+    while True:
+        event = conn.recvEvent()
+        event_name = event.getHeader("Event-Name")
+
+        if event and event_name == "CHANNEL_PARK":
+            uuid = event.getHeader("Unique-ID")
+            logger.info("Call %s started", uuid)
+            conn.api("uuid_answer", uuid)
+
+        if event and event_name == "CHANNEL_ANSWER":
+            uuid = event.getHeader("Unique-ID")
+            logger.info("Call %s answered", uuid)
+            connect_channel_with_ws_endpoint(
+                conn, uuid, config.get('WS_ENDPOINT'))
+            logger.info("Connected call %s to WS endpoint", uuid)
+
+        if event and event_name == 'CUSTOM':
+            log_recognition_result(event)
+
+        if event and event_name == "CHANNEL_HANGUP":
+            uuid = event.getHeader("Unique-ID")
+            logger.info("Call %s ended", uuid)
+
+
+def start_esl_server(host, port):
+    """
+    Start ESL server to listen for incoming connections from FreeSWITCH.
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((host, port))
+        server_socket.listen()
+        logger.info("ESL server listening on %s:%s", host, port)
+        while True:
+            client_socket, addr = server_socket.accept()
+            thread = threading.Thread(
+                target=handle_esl_connection, args=(client_socket, addr))
+            thread.start()
 
 
 def connect_to_freeswitch():
@@ -109,14 +159,15 @@ def log_recognition_result(event):
     Returns:
         None
     """
-    if event.getType() == 'CUSTOM':
-        try:
-            result = json.loads(event.getBody())
-            if len(result['partial']) > 0:
-                logger.info("Partial recognition result: %s",
-                            result['partial'])
-        except (json.JSONDecodeError, KeyError):
-            pass
+    if not event:
+        return
+    try:
+        result = json.loads(event.getBody())
+        if len(result['partial']) > 0:
+            logger.info("Partial recognition result: %s",
+                        result['partial'])
+    except (json.JSONDecodeError, KeyError):
+        pass
 
 
 def main():
@@ -128,14 +179,13 @@ def main():
         logger.error('Exiting...')
         sys.exit(1)
 
-    uuid = originate_call(esl_conn, config.get('SIP_EXTENSION'))
-    connect_channel_with_ws_endpoint(esl_conn, uuid, config.get('WS_ENDPOINT'))
+    esl_inbound_host = config.get('ESL_INBOUND_HOST', '0.0.0.0')
+    esl_inbound_port = int(config.get('ESL_INBOUND_PORT', '8022'))
 
-    esl_conn.events("plain", "ALL")
-    while True:
-        event = esl_conn.recvEvent()
-        if event:
-            log_recognition_result(event)
+    start_esl_server(esl_inbound_host, esl_inbound_port)
+
+    # uuid = originate_call(esl_conn, config.get('SIP_EXTENSION'))
+    # connect_channel_with_ws_endpoint(esl_conn, uuid, config.get('WS_ENDPOINT'))
 
 
 if __name__ == "__main__":
