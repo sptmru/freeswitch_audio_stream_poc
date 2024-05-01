75ac3fc1a4d0ca8beddaad01e24c3c5b0efafcc5
Soslan Aldatov <soslanaldatov@gmail.com>
2024-05-02 01:05:58 +0400

    change FreeSWITCH RTP ports and expose these ports in Docker Compose configuration

--- freeswitch/entrypoint.sh
+++ freeswitch/entrypoint.sh
@@ -2,6 +2,10 @@
 
 sed -i '/<X-PRE-PROCESS cmd="set" data="default_password=[^"]*"\/>/c\<X-PRE-PROCESS cmd="set" data="default_password='"${EXTENSION_PASSWORD:-extensionpassword}"'"\/>' /usr/local/freeswitch/conf/vars.xml
 
+sed '/<!-- <param name="rtp-end-port" value="32768"\/> -->/a \
+<param name="rtp-start-port" value="10000"/>\n<param name="rtp-end-port" value="10010"/>' \
+-i /usr/local/freeswitch/conf/autoload_configs/switch.conf.xml
+
 NUMBER_TO_DIAL="${NUMBER_TO_DIAL:-99999}"
 
 awk -v numbertodial="$NUMBER_TO_DIAL" '
